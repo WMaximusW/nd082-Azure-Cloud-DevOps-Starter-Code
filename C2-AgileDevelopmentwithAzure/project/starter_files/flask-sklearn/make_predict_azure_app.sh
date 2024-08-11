2735034c051dc6eb2c4452c49bb40356f365bac0
WMaximusW <wnguyenquoctrung1993w@gmail.com>
2024-08-11 10:10:38 +0700

    update: evident

--- C2-AgileDevelopmentwithAzure/project/starter_files/flask-sklearn/make_predict_azure_app.sh
+++ C2-AgileDevelopmentwithAzure/project/starter_files/flask-sklearn/make_predict_azure_app.sh
@@ -5,25 +5,28 @@ echo "Port: $PORT"
 
 # POST method predict
 curl -d '{
-   "CHAS":{
-      "0":0
+   "CHAS": {
+      "0": 0
    },
-   "RM":{
-      "0":6.575
+   "RM": {
+      "0": 6.575
    },
-   "TAX":{
-      "0":296.0
+   "TAX": {
+      "0": 296.0
    },
-   "PTRATIO":{
-      "0":15.3
+   "PTRATIO": {
+      "0": 15.3
    },
-   "B":{
-      "0":396.9
+   "B": {
+      "0": 396.9
    },
-   "LSTAT":{
-      "0":4.98
+   "LSTAT": {
+      "0": 4.98
    }
-}'\
-     -H "Content-Type: application/json" \
-     -X POST https://<yourappname>.azurewebsites.net:$PORT/predict 
-     #your application name <yourappname>goes here
+}' \
+-H "Content-Type: application/json" \
+-X POST "https://trungnq72-project2-fubufxbgb7hab7e0.eastus-01.azurewebsites.net/predict" \
+-v  # This makes curl verbose and shows what's happening
+
+# Prevent the script from closing
+read -p "Press any key to exit..."
